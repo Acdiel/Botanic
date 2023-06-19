7f31c399b0a143dcac2ec72de70ea9977060bfe5
LucasRobles2323 <lucasrobles2323@gmail.com>
2023-06-19 13:32:38 -0400

    Agruegue las operaciones (suma, resta, multiplicación y division)

--- Botanic/src/MAIN/MyVisitor.java
+++ Botanic/src/MAIN/MyVisitor.java
@@ -25,6 +25,64 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 		String nombre = nodoTerminal.getClass().getSimpleName();
 		return nombre.substring(0, nombre.length() - 7);
 	}
+
+	public boolean isNum(String variable)
+	{
+		try {
+            Integer.parseInt(variable);
+			return true;
+        } catch (NumberFormatException excepcion) { }
+
+		try {
+            Double.parseDouble(variable);
+			return true;
+        } catch (NumberFormatException excepcion) { }
+
+		return false;
+	}
+
+	public void realizarGuardarOperacion(String value1, String value2, String operador, String key){
+		Double valor1 = Double.parseDouble(value1);
+		Double valor2 = Double.parseDouble(value2);
+		Double valorFinal = 0.0;
+		
+		if(operador.equals("primavera"))
+		{
+			valorFinal = valor1 + valor2;
+			variables.put("operacion" + key, valor1 + " + " +valor2);;
+		}
+		else if(operador.equals("invierno"))
+		{
+			valorFinal = valor1 - valor2;
+			variables.put("operacion" + key, valor1 + " - " +valor2);;
+		}
+		else if(operador.equals("injertar"))
+		{
+			valorFinal = valor1 * valor2;
+			variables.put("operacion" + key, valor1 + " * " +valor2);;
+		}
+		else if(operador.equals("transplantar"))
+		{
+			if(valor2 == 0.0){
+				System.out.println("No se puede tener 0.0 de divisor.");
+				variables.put(key, null);
+				variables.put("operacion" + key, valor1 + " / " +valor2);
+				return;
+			}
+			valorFinal = valor1 / valor2;
+			variables.put("operacion" + key, valor1 + " / " +valor2);
+		}
+		else{
+			System.out.println("No existe el operador.");
+			variables.put(key, null);;
+			return;
+		}
+
+		String resultado = String.format("%.4f", valorFinal);
+		// Transforma la coma en punto (Double es con punto)
+		resultado = resultado.replace(",", ".");
+		variables.put(key, resultado);
+	}
 	
 	@Override
 	public Integer visitLectura(ParserTParser.LecturaContext ctx) {
@@ -45,7 +103,6 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 		String key = variablesKeyInv.get(ctx.getText());
 		
 		variables.put(key, ctx.getChild(1).getText());
-		//System.out.println(key + " = "+ ctx.getChild(1).getText()); // Muestra lo guardado
 
 		return visitChildren(ctx);
 	}
@@ -55,9 +112,11 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 	{
 		// Obtiene la llave de la variable
 		String key = variablesKeyInv.get(ctx.getText());
-		
-		variables.put(key, ctx.getChild(1).getText());
-		//System.out.println(key + " = "+ ctx.getChild(1).getText()); // Muestra lo guardado
+
+		String value = ctx.getChild(1).getText();
+		// Transforma la coma en punto (Double es con punto)
+		value = value.replace(",", ".");
+		variables.put(key, value);
 
 		return visitChildren(ctx);
 	}
@@ -67,10 +126,39 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 	{
 		// Obtiene la llave de la variable
 		String key = variablesKeyInv.get(ctx.getText());
+
+		String value = ctx.getChild(1).getText();
+		// Le quita las comillas antes de guardarlo
+		value = value.replace("\"", "");
 		
-		variables.put(key, ctx.getChild(1).getText());
-		//System.out.println(key + " = "+ ctx.getChild(1).getText()); // Muestra lo guardado
+		variables.put(key, value);
 		
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Integer visitOperacion(ParserTParser.OperacionContext ctx)
+	{
+		String key = ctx.getChild(0).getText(); // Donde se guarda el resultado
+		String value1 = variables.get( ctx.getChild(2).getText() ); // 1era variable de la operacion
+		String operador = ctx.getChild(4).getText(); // operador
+		String value2 = variables.get( ctx.getChild(6).getText() ); // 2da variable de la operacion
+		
+		if (!isNum(value1))
+		{
+			System.out.println(ctx.getChild(2).getText() + " no es un numero. No se puede realizar la operación.");
+			variables.put(key, null);
+			return visitChildren(ctx);
+		}
+		if (!isNum(value2))
+		{
+			System.out.println(ctx.getChild(6).getText() + " no es un numero. No se puede realizar la operación.");
+			variables.put(key, null);
+			return visitChildren(ctx);
+		}
+		
+		realizarGuardarOperacion(value1, value2, operador, key);
+
 		return visitChildren(ctx);
 	}
 
@@ -78,9 +166,8 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 	@Override
 	public Integer visitImpresion(ParserTParser.ImpresionContext ctx)
 	{
-		String output = variables.get( ctx.getChild(2).getText() );
-		System.out.println(output);
-
+		System.out.println(variables.get(ctx.getChild(2).getText()));
+		
 		return visitChildren(ctx);
 	}
 }
